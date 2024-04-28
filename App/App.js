cfa941e5fb175f0de0c59f70ee3ff6097187e75a
mchanfiou <mimous92@hotmail.fr>
2024-04-28 15:24:58 +0200

    2e commit

--- App/App.js
+++ App/App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-export default function App() {
+import Home from './CustomPages/home';
+import List from './CustomPages/list';
+import Aboutus from './CustomPages/aboutus';
+import Character from './CustomPages/character';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function StackNavigator() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false }}>
+      <Stack.Screen name="Acceuil" component={Home} />
+      <Stack.Screen name="Les Personnages" component={List}/>
+      <Stack.Screen name="A propos" component={Aboutus}/> 
+      <Stack.Screen name="Personnage" component={Character}/>
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator  screenOptions={{headerStyle: { backgroundColor: '#3B5998'}, tabBarStyle:{backgroundColor: '#3B5998'}, tabBarInactiveTintColor: 'black', tabBarActiveTintColor: '#c0c0c0' }} tabBarOptions={{}}>
+        <Tab.Screen name="Accueil" component={StackNavigator} />
+        <Tab.Screen name="Les Personnages" component={List}/>
+        <Tab.Screen name="A propos" component={Aboutus}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+
+export default App;
